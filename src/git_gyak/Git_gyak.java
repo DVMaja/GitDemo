dd946a1f78984c92a76126356525fcc8488e74a7
DVMaja <majadreilinger@gmail.com>
2023-09-20 12:51:01 +0200

    elágazás

--- src/git_gyak/Git_gyak.java
+++ src/git_gyak/Git_gyak.java
@@ -4,6 +4,10 @@ public class Git_gyak {
 
         public static void main(String[] args) {
             for (int i = 0; i < 10; i++) {
+                if (true) {
+                    
+                    
+                }
                 
             }
         
