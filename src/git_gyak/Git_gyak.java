bd6f4ff67b7d650d67cfa7610c41f720443451ef
DVMaja <majadreilinger@gmail.com>
2023-09-20 12:57:37 +0200

    static methodusból nem static methodus

--- src/git_gyak/Git_gyak.java
+++ src/git_gyak/Git_gyak.java
@@ -5,7 +5,7 @@ public class Git_gyak {
         public static void main(String[] args) {
             for (int i = 0; i < 10; i++) {
                 if (true) {
-                    Methodus();
+                    //Methodus();
                     
                 }
                 
@@ -13,7 +13,7 @@ public class Git_gyak {
         
     }
 
-    private static void Methodus() {
+    private void Methodus() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
