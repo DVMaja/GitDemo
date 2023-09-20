82430239b239c73c196aab1fc0dc07da80ba26c0
DVMaja <majadreilinger@gmail.com>
2023-09-20 12:50:42 +0200

    for ciklus

--- src/git_gyak/Git_gyak.java
+++ src/git_gyak/Git_gyak.java
@@ -3,6 +3,9 @@ package git_gyak;
 public class Git_gyak {
 
         public static void main(String[] args) {
+            for (int i = 0; i < 10; i++) {
+                
+            }
         
     }
     
